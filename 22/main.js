d2b1de3fbd0910aff8cea9d96f179f27407060c6
Mitlond <comp122012016@mail.ru>
2016-09-15 10:55:09 +0300

    JS

---
+++ 22/main.js
@@ -0,0 +1,72 @@
+/*function res( ) {
+	var one=1;
+	var two=2;
+	var tri=3;
+	sum=one+two+tri;
+	return sum;
+}
+console.log(res());
+
+function raz( ) {
+	var parseInt='текст47';
+	return parseInt;
+}
+console.log(parseInt());
+function zov (obj,objName) {
+	var  result= "4";
+  for (var i in obj) {
+    if (obj.(i)) {
+        result += objName + "." + i + " = " + obj[i] + "\n";
+    }
+  }
+  return result;
+}
+console.log(typeof(objName));*/
+
+	switch (a<0) {
+
+	  case 3:
+
+	    break
+
+	  case 4:
+	    
+	    break
+
+	  case 5:
+
+	    break
+
+	  default:
+	} 
+	console.log(a<0); 
+	
+	/*function res(x){
+	var x=7;
+	var y=3;
+	while(7+3)
+		x+y;
+	do (x+y);
+	while(7+3);
+	for(var x=0;x<78;i++);
+	
+	}
+	console.log(x);*/ //не правильный возможно код:)
+	
+for(var i = 3;i < 78; i++){
+	}
+console.log(i);
+
+var i = 78;
+do {
+   ( i );
+  i++;
+} while (i < 3);
+console.log(i);
+
+	var i = 78;
+while (i < 3) {
+  ( i );
+  i++;
+}
+console.log(i);
